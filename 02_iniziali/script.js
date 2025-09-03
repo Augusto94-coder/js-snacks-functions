1d3281438c2f10160cec5609693c05a832432dfa
Augusto94-coder <augustodileo94@gmail.com>
2025-09-03 19:19:03 +0200

    02-iniziali svolto

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,26 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function getInitialsClassic(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i].charAt(0));
+  }
+  return result;
+}
+
+const getInitialsArrow = (arr) => {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i][0]); 
+  }
+  return result;
+};
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(getInitialsClassic(names));
+console.log(getInitialsArrow(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
