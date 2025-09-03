9a627118f521b64ebb9640aeea5baaefe567d24e
Augusto94-coder <augustodileo94@gmail.com>
2025-09-03 19:57:53 +0200

    04-conta-vocali svolto

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,36 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let count = 0;
 
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i].toLowerCase())) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+const countVowelsArrow = (str) => {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i].toLowerCase())) {
+      count++;
+    }
+  }
+
+  return count;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
+/* console.log(`La parola "${word}" contiene ${countVowels(word)} vocali.`); */
+console.log("La parola " + word + " contiene " + countVowels(word) +  " vocali")
+console.log("La parola " + word + " contiene " + countVowelsArrow(word) +  " vocali")
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
